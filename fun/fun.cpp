c449d750f6c9137052f71c1ca85565e61d2a78a0
vladStar223 <vlad32kolm@gmail.com>
2024-11-13 17:56:40 +1000

    22

--- fun/fun.cpp
+++ fun/fun.cpp
@@ -6,31 +6,35 @@
 #include <random>
 
 using namespace std;
-double s_triangle(&arr[2]) {
+double s_triangle(double(&a)[2], double(&b)[2], double(&c)[2]){
+
+	return 0.5*(a[0]*(b[1]-c[1])+b[0]*(c[1]-a[1])+c[0]*(a[1]-b[1]));
+}
+vector<double>(2) half_cor(double(&a)[2]) {
+	vector<double>s(2);
 
 }
+
 int main()
 {
 	srand(time(NULL));
-	double a[1];
-	double b[1];
-	double c[1];
+	double a[2];
+	double b[2];
+	double c[2];
 	string v = "ABC";
 	for (int i = 0; i < 3; i++) {
-		do
-		{
-			cout << "Input x y" <<v[i]<< endl;
-			if (i == 0) {
-				cin >> a[1] >> a[1];
-			}
-			else if (i == 1) {
-				cin >> b[1] >> b[1];
-			}
-			else if (i == 2) {
-				cin >> c[1] >> c[1];
-			}
+		cout << "Input x y" << v[i] << endl;
+		if (i == 0) {
+			cin >> a[0]>> a[1];
 
-		} while (true);
+		}
+		else if (i == 1) {
+			cin >> b[0] >> b[1];
+		}
+		else if (i == 2) {
+			cin >> c[0] >> c[1];
+		}
+
 	}
 
 
