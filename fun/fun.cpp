9afd425cd04108e4465a8da051addebde34eabd4
vladStar223 <vlad32kolm@gmail.com>
2024-11-13 17:41:38 +1000

    ff

--- fun/fun.cpp
+++ fun/fun.cpp
@@ -6,12 +6,32 @@
 #include <random>
 
 using namespace std;
+double s_triangle(&arr[2]) {
+
+}
 int main()
 {
 	srand(time(NULL));
 	double a[1];
 	double b[1];
 	double c[1];
+	string v = "ABC";
+	for (int i = 0; i < 3; i++) {
+		do
+		{
+			cout << "Input x y" <<v[i]<< endl;
+			if (i == 0) {
+				cin >> a[1] >> a[1];
+			}
+			else if (i == 1) {
+				cin >> b[1] >> b[1];
+			}
+			else if (i == 2) {
+				cin >> c[1] >> c[1];
+			}
+
+		} while (true);
+	}
 
 
 
